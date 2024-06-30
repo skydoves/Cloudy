2b1ecbcfbaa92ea1f8e9a9625cf1caf702cbd9d5
skydoves <skydoves@naver.com>
2024-06-30 20:31:28 +0900

    Refactor BaselineProfileGenerator

--- benchmark/src/main/java/com/skydoves/cloudy/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/java/com/skydoves/cloudy/benchmark/BaselineProfileGenerator.kt
@@ -15,17 +15,20 @@
  */
 package com.skydoves.cloudy.benchmark
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import org.junit.Rule
 import org.junit.Test
 
+@RequiresApi(Build.VERSION_CODES.P)
 class BaselineProfileGenerator {
   @get:Rule
   val baselineProfileRule = BaselineProfileRule()
 
   @Test
   fun startup() =
-    baselineProfileRule.collectBaselineProfile(
+    baselineProfileRule.collect(
       packageName = targetPackage
     ) {
       pressHome()
