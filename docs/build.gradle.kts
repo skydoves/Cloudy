9836af403f5614c0aa68a3895f20dec30fc312e2
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-12-16 01:25:53 +0900

    chore: add docs module

---
+++ docs/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+/*
+ * Designed and developed by 2022 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  id(libs.plugins.kotlin.multiplatform.get().pluginId)
+  id(libs.plugins.compose.multiplatform.get().pluginId)
+  id(libs.plugins.compose.compiler.get().pluginId)
+}
+
+kotlin {
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs {
+    browser {
+      commonWebpackConfig {
+        outputFileName = "cloudy-docs.js"
+      }
+    }
+    binaries.executable()
+  }
+
+  sourceSets {
+    commonMain.dependencies {
+      implementation(compose.components.resources)
+    }
+
+    wasmJsMain.dependencies {
+      implementation(project(":cloudy"))
+
+      implementation(compose.runtime)
+      implementation(compose.foundation)
+      implementation(compose.ui)
+      implementation(compose.material3)
+      implementation(compose.materialIconsExtended)
+
+      implementation(libs.landscapist.coil)
+      implementation(libs.coil)
+      implementation(libs.coil.network)
+      implementation(libs.ktor.client.js)
+    }
+  }
+}
