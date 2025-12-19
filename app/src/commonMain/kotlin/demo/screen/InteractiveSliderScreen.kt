eb6d23e2ce0cc3cf3b14cc1795793c70cec5b0d4
Ambarish Manna <amanna1041@gmail.com>
2025-12-19 22:52:26 +0530

    refactor: migrate Slider component from Material 2 to Material 3

--- app/src/commonMain/kotlin/demo/screen/InteractiveSliderScreen.kt
+++ app/src/commonMain/kotlin/demo/screen/InteractiveSliderScreen.kt
@@ -28,8 +28,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material.Slider
-import androidx.compose.material.SliderDefaults
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
