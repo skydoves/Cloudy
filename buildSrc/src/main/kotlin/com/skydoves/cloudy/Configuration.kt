73c85189e4666504026f854fb5ce3d5ea2694af5
skydoves <skydoves2@gmail.com>
2026-06-28 07:34:07 +0900

    Prepare for release 0.6.1

--- buildSrc/src/main/kotlin/com/skydoves/cloudy/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/cloudy/Configuration.kt
@@ -6,9 +6,9 @@ object Configuration {
   const val minSdk = 23
   const val majorVersion = 0
   const val minorVersion = 6
-  const val patchVersion = 0
+  const val patchVersion = 1
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 15
+  const val versionCode = 16
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
